99dcce62f2d4509336031da87237e8657cbf7d1f
pierluigigiancola <pierluigi.giancola@gmail.com>
2020-04-19 12:28:05 +0200

    Remove Props when building lib

--- .babelrc.js
+++ .babelrc.js
@@ -1,4 +1,4 @@
-module.exports = api => {
+module.exports = (api) => {
   //env from parameter --env-name, you can customize your build based on env.
   //example command: npx babel src --out-dir lib --env-name 'dev'
   //https://babeljs.io/docs/en/options#envname
@@ -7,16 +7,23 @@ module.exports = api => {
   //Babel complains about caches and tells him what to do is mandatory
   //https://babeljs.io/docs/en/config-files#apicache
   //If set to false this configuration runs every time on each file it transpile.
-  api.cache(true);
+  api.cache(true)
   return {
     presets: [
-      ['@babel/preset-env',
+      [
+        '@babel/preset-env',
         {
           modules: false,
-          loose: true
-        }
+          loose: true,
+        },
       ],
-      '@babel/preset-react'
-    ]
-  };
-};
+      '@babel/preset-react',
+    ],
+    plugins: [
+      [
+        'transform-react-remove-prop-types',
+        { ignoreFilenames: ['node_modules'], removeImport: true },
+      ],
+    ],
+  }
+}
